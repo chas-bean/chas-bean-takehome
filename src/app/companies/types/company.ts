79c03c7d9a9c4184c7b98c040207d15386804e5f
Chas Bean <charlesparkerbean@gmail.com>
2023-05-29 15:58:08 -0400

    Add constant for JSON companies

---
+++ src/app/companies/types/company.ts
@@ -0,0 +1,10 @@
+export type Company = {
+  ein: Number;
+  company_name: String;
+  plan_year: Number;
+  employee_count: Number;
+  company_state: String;
+  premium_sum: Number;
+  broker_commission_sum: Number;
+  participants_sum: Number;
+};
